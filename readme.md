7dbce1598e679b1ad3a51830534dbd1a51ff9d1d
callidaria <lo.true@gmx.de>
2024-10-19 11:38:46 +0200

    fixing build & execution process + note to self

--- readme.md
+++ readme.md
@@ -4,5 +4,11 @@ Projekttemplate für die Abgaben der Übungsaufgaben. Siehe:
 
 - https://tramberend.bht-berlin.de/course/cgg
 
+# Comprehensive HowTo
 
+To generate images execute:
 
+```
+bash run.sh
+```
+The resulting files will appear in ./images
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 export CLASSPATH="bin:lib/com.mokiat.data.front-2.0.1.jar"
-export invoke="javac -d bin `fdfind .java src`"
+export invoke="javac -d bin `fdfind .java src` && java -cp bin cgg.a01.Main"
 echo $invoke
 eval $invoke
