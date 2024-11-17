37a28f468c0e02ac67874d5451c4036bba36835e
callidaria <lo.true@gmx.de>
2024-11-17 17:34:23 +0100

    why oh god why is my importance sampling broken!

--- src/cgg/RayTracer.java
+++ src/cgg/RayTracer.java
@@ -19,6 +19,7 @@ public class RayTracer implements Sampler
 	private Stage scene;
 	private final Color background = color(0,0,0);
 	private final Color error = color(0,.9,1);
+	private final ImageTexture LUT_BRDF = new ImageTexture("./res/lut/brdf.png");
 
 	public RayTracer(Stage scene)
 	{
@@ -28,19 +29,29 @@ public class RayTracer implements Sampler
 	public Color getColor(Vec2 coord)
 	{
 		Ray __Ray = scene.camera().generateRay(coord);
+		return _processScene(__Ray,0);
+	}
+
+
+	private Color _processScene(Ray ray,int depth)
+	{
+		if (depth>4) return color(0,0,0);
 		Queue<HitTuple> __Hits = new LinkedList<>();
 
 		// emitter
-		for (Geometry g : scene.emitter())
+		if (depth<1)
 		{
-			Queue<HitTuple> __Proc = g.intersect(__Ray);
-			if (recentGeometry(__Hits,__Proc)) __Hits = __Proc;
+			for (Geometry g : scene.emitter())
+			{
+				Queue<HitTuple> __Proc = g.intersect(ray);
+				if (recentGeometry(__Hits,__Proc)) __Hits = __Proc;
+			}
 		}
 
 		// opaque geometry
 		for (Geometry g : scene.objects())
 		{
-			Queue<HitTuple> __Proc = g.intersect(__Ray);
+			Queue<HitTuple> __Proc = g.intersect(ray);
 			if (recentGeometry(__Hits,__Proc)) __Hits = __Proc;
 		}
 
@@ -49,7 +60,7 @@ public class RayTracer implements Sampler
 		Hit __Recent = __Hits.peek().front();
 		Color __Result = switch (__Recent.material())
 		{
-		case PhysicalMaterial c -> _shadePhysical(__Recent);
+		case PhysicalMaterial c -> _shadePhysical(__Recent,ray,depth);
 		case SurfaceMaterial c -> _shadePhong(__Recent);
 		case SurfaceColour c -> _shadeLaemp(__Recent);
 		default -> error;
@@ -58,6 +69,7 @@ public class RayTracer implements Sampler
 		// colour correction
 		__Result = subtract(color(1.),exp(multiply(__Result,-scene.camera().exposure())));
 		return pow(__Result,1./2.2);
+		// FIXME multiple colour corrections when bouncing
 	}
 
 	private Color _shade(Hit hit)
@@ -68,16 +80,18 @@ public class RayTracer implements Sampler
 		return add(ambient,diffuse);
 	}
 
-	private Color _shadePhysical(Hit hit)
+	private Color _shadePhysical(Hit hit,Ray ray,int depth)
 	{
 		// extract colour information
+		// colour preferredly to be a constant colour because the loader does not translate into sRGB colourspace
+		// FIXME or else albedo textures are utterly useless
 		Color p_Colour = hit.material().getComponent(MaterialComponent.COLOUR,hit);
 		Color p_Material = hit.material().getComponent(MaterialComponent.MATERIAL,hit);
 
 		// translate colour to surface info
 		double __Metallic = p_Material.r();
 		double __Roughness = p_Material.g();
-		double __Occlusion = p_Material.b();
+		double __Cavity = p_Material.b();
 
 		// precalculations
 		double aSq = pow(__Roughness,4.);
@@ -122,8 +136,68 @@ public class RayTracer implements Sampler
 			lR = multiply(multiply(lR,vec3(p_Light.physicalInfluence(hit.position()))),__dtLightIn);
 			__Result = add(__Result,lR);
 		}
+		Color out = color(__Result);
 
-		return color(__Result);
+		// gi (testing)
+		// ...fresnel again (probata et commendatae!)
+		// the HLSL code uses saturate() (dumb name) here, but max is all we need, dot product is never <0
+		double __Attitude = max(dot(hit.normal(),__CameraDir),.0);
+		Vec3 __GIFresnel = subtract(max(vec3(1.-__Roughness),__Fresnel0),__Fresnel0);
+		__GIFresnel = multiply(__GIFresnel,pow(clamp(1.-__Attitude,.0,1.),5.));
+		__GIFresnel = add(__Fresnel0,__GIFresnel);
+
+		// integral lookup table
+		// table source from epic games has been flipped to conform with tex coords starting in the upper left
+		// how much does this save? correct: almost nothing (single signswap), but we do what we can over here
+		// and yes i just ripped it from the paper directly instead of computing it, no time for shenanigansry
+		Color __LUT = LUT_BRDF.getColor(vec2(__Attitude,__Roughness));
+
+		// sampling from the environment
+		// cant do this in a lookup table can you?
+		Vec3 __GIResult = vec3(0);
+		Vec3 __R = subtract(multiply(2*__Attitude,hit.normal()),__CameraDir);
+		double __SmpWeight = .0;  // dont forget this one, the goddamn paper forgets to declare this one!
+		final int SAMPLES = 1;
+		for (int i=0;i<SAMPLES;i++)
+		{
+			// fpd avoidance trickery (there is a book with this hack & its generally used)
+			// bitshifting in java is a different kind of adventure
+			int __VDCorput = (i<<16)|(i>>>16);
+			__VDCorput = ((__VDCorput&0x55555555)<<1|(__VDCorput&0xAAAAAAAA)>>>1);
+			__VDCorput = ((__VDCorput&0x33333333)<<2|(__VDCorput&0xCCCCCCCC)>>>2);
+			__VDCorput = ((__VDCorput&0x0F0F0F0F)<<4|(__VDCorput&0xF0F0F0F0)>>>4);
+			__VDCorput = ((__VDCorput&0x00FF00FF)<<8|(__VDCorput&0xFF00FF00)>>>8);
+			Vec2 __Hammersley = vec2(
+					(float)i/(float)SAMPLES,
+					Float.intBitsToFloat(__VDCorput)*2.3283064365386963e-10
+				);
+
+			// importance sample (your lobez quark! where is my oomox after implementing this huh?)
+			// the paper regenerates our aSq in two steps? we are just gonna reuse it, just aSqing questions!
+			double phi = 2*PI*__Hammersley.y();
+			double thCos = sqrt((1-__Hammersley.y())/(1+(aSq-1)*__Hammersley.y()));
+			double thSin = sqrt(1-pow(thCos,2.));
+			Vec3 __IS = vec3(thSin*cos(phi),thSin*sin(phi),thCos);
+			Vec3 up = (abs(__R.z())<.999) ? vec3(0,0,1) : vec3(1,0,0);
+			Vec3 xTan = normalize(cross(up,__R));
+			Vec3 yTan = cross(__R,xTan);
+			__IS = add(multiply(xTan,__IS.x()),multiply(yTan,__IS.y()),multiply(__R,__IS.z()));
+
+			// samples generate lobecast
+			Vec3 __PEnvLight = subtract(multiply(2*dot(__CameraDir,__IS),__IS),__CameraDir);
+			double __DEnvLight = max(dot(hit.normal(),__PEnvLight),.0);
+			if (__DEnvLight<.0001) continue;
+			Ray __GIR = new Ray(hit.position(),__PEnvLight,.001,1000.);
+			__GIResult = add(__GIResult,multiply(vec3(_processScene(__GIR,++depth)),__DEnvLight));
+			__SmpWeight += __DEnvLight;
+		}
+
+		// convolute samples and mix
+		Vec3 __GI = divide(__GIResult,__SmpWeight);
+		__GI = multiply(__GI,add(multiply(__GIFresnel,__LUT.r()),__LUT.g()));
+		out = mix(out,color(__GI),.5);
+
+		return out;
 	}
 
 	private double _schlickBeckmannApprox(double l,double r)
